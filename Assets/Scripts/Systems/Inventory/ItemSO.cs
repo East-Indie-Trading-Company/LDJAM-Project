60d93fef8389761db3fc19311b75f8214dea40e9
Solomon Lucas <soluke22@gmail.com>
2025-10-04 11:16:00 -0400

    [Trading] Create Item Scriptable Object

---
+++ Assets/Scripts/Systems/Inventory/ItemSO.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Trading
+{
+
+    [CreateAssetMenu(menuName = "Trading/Items/ItemSO", fileName = "ItemSO_")]
+    public class ItemSO : ScriptableObject
+    {
+        /// <summary>
+        /// The name, description, and icon of the item. Visible features to be shown in UI.
+        /// </summary>
+        [Header("Display")]
+        public string itemName;
+        [TextArea] public string description;
+        public Sprite icon;
+
+        /// <summary>
+        /// The base value and category of the item. These are used for trading calculations.
+        /// </summary>
+        [Header("Base Economy Anchor")]
+        [Min(0)] public int baseValue = 1;    // Towns will override per-town
+
+        public ItemCategory category = ItemCategory.Material;
+        public string[] tags;
+    }
+
+    public enum ItemCategory { Weapon, Food, Material, Potion, Misc }
+}
