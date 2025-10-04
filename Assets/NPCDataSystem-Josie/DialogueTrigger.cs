35353b903ca7676065d6cd830cbe7a8d975999b1
Josephine Emenya <115475933+josieemenya@users.noreply.github.com>
2025-10-04 17:25:07 +0100

    NPC data system so far
    done all I can do without other dependencies

---
+++ Assets/NPCDataSystem-Josie/DialogueTrigger.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+
+public class
+public class DialogueTrigger : MonoBehaviour
+{
+    
+    public NPCData npcData;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/NPCDataSystem-Josie/NPCData.cs
+++ Assets/NPCDataSystem-Josie/NPCData.cs
@@ -1,14 +1,90 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NPCData", menuName = "Scriptable Objects/NPCData")]
+
+public enum GreetingFlag
+{
+    None,
+    Friendly,
+    Neutral,
+    Hostile
+}
 public class NPCData : ScriptableObject
 {
-    [SerializeField] private string npcName;
-    [SerializeField] private Texture2D npcPotraitID;
+    [SerializeField] string npcName;
+    [SerializeField] Texture2D npcPotraitID;
     
-    [SerializeField] private string[] npcDialogueLines; // might change to a Dictionary later
-    [SerializeField] private string[] npcRumorDialogueLines; // might change to a Dictionary later
+    //DialogueConversation - vars : hasPlayed, ; methods : Trigger()
+    [SerializeField] DialogueConversation[] npcDialogueLines; 
+    [SerializeField] DialogueConversation[] npcRumorDialogueLines; 
+    
+    
+    [SerializeField] Dictionary<GreetingFlag, string> npcGreetingDialogueLines;
+    
+    privat
     
     // insert Reputation Impact, whatever that is here
     [SerializeField] private int npcReputationImpact;
+    
+    void Talk()
+    {
+        // Check FLag, check priority of dialogue lines, then output Dialogue
+        Array.ForEach(npcDialogueLines, line => {
+                    if (!line.hasPlayed)
+                    {
+                        if (line.hasFlag)
+                        {
+                            //Check Priority of Dialogue Lines
+                            // Trigger Dialogue System to start conversation with this NPC
+                            line.hasPlayed = true; // Set hasPlayed to true after triggering
+                        }
+                        return;
+                    }
+                }
+            );
+    }
+    
+    void Rumor()
+    {
+       Array.ForEach(npcRumorDialogueLines, line => {
+                    if (!line.hasPlayed)
+                    {
+                        if (line.hasFlag)
+                        {
+                            //Check Priority of Dialogue Lines
+                            // Trigger Dialogue System to start conversation with this NPC
+                            line.hasPlayed = true; // Set hasPlayed to true after triggering
+
+                        }
+                        return;
+                    }
+                }
+            );
+    }
+
+    void Greeting()
+    { 
+        Array.ForEach(npcGreetingDialogueLines, greet => {
+                    // Trigger Dialogue System to start conversation with this NPC
+
+                    switch (greet.Key)
+                    {
+                        case GreetingFlag.Friendly:
+                            // Friendly Greeting Logic
+                            break;
+                        case GreetingFlag.Neutral:
+                            // Neutral Greeting Logic
+                            break;
+                        case GreetingFlag.Hostile:
+                            // Hostile Greeting Logic
+                            break;
+                        default:
+                            // Default Greeting Logic
+                            break;
+                    }
+
+            }
+            );
+    }
+
 }
