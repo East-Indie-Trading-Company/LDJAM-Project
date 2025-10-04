ba76c85669a62a7802b0eb6d636578a58db1ff7e
ProfessionalCodeMaster <RMartinez7771@yahoo.com>
2025-10-04 14:15:07 -0400

    Placeholder Map, Towns, and Buttons Created
    I created the placeholder map, placeholder towns, and buttons in the Map scene. I also created the MapManager script.

---
+++ Assets/Scripts/Managers/MapManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MapManager : MonoBehaviour
+{
+    // Makes the camera zoom out to show the full map.
+    public void TravelBackToMap()
+    {
+        
+    }
+    
+    // Each of these functions makes the camera zoom in to each corresponding town on the map.
+    public void TravelToTown1()
+    {
+        
+    }
+    public void TravelToTown2()
+    {
+        
+    }
+    public void TravelToTown3()
+    {
+        
+    }
+    public void TravelToTown4()
+    {
+        
+    }
+    public void TravelToTown5()
+    {
+
+    }
+    public void TravelToTown6()
+    {
+
+    }
+    public void TravelToTown7()
+    {
+
+    }
+}
