e26e79ab4210573719d49d7ac343b2d19871fd6f
abcarso <100879194+ArwenSylveck@users.noreply.github.com>
2025-10-04 23:53:27 -0400

    [DemoScene] Add demo functionality

--- Assets/NPCDataSystem-Josie/DialogueTrigger.cs
+++ Assets/NPCDataSystem-Josie/DialogueTrigger.cs
@@ -1,21 +1,49 @@
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    
+    [Header("NPC DATA")]
     public NPCData npcData = null;
-    
-    
+    [Header("UI References")]
+    [SerializeField] TextMeshProUGUI npcNameText; // To be implemented
+    [SerializeField] Image npcPortrait;
+    [SerializeField] TextMeshProUGUI townNameText; // To be implemented
+    [SerializeField] TextMeshProUGUI npcText;
+    [SerializeField] Button chatButton;
+    [SerializeField] Button rumorButton;
+    [SerializeField] GameObject marketCanvas;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        chatButton.onClick.AddListener(onChat);
+        rumorButton.onClick.AddListener(onRumor);
+        DisplayTown(); // VERY SUPER TEMOIRARY
     }
 
-    // Update is called once per frame
-    void Update()
+    void DisplayTown()
     {
-        
+        if (npcData != null)
+        {
+            npcPortrait.sprite = npcData.displayInfo.npcIcon;
+            npcData.Greeting(npcText);
+        }
+        else
+        {
+            Debug.LogWarning("[DialogueTrigger] NPC data is missing!");
+        }
+    }
+
+    void onChat()
+    {
+        marketCanvas.SetActive(false);
+        npcData.Talk();
+    }
+    void onRumor()
+    {
+        marketCanvas.SetActive(false);
+        npcData.Rumor();
     }
 }
