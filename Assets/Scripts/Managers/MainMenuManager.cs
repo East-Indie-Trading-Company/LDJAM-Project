6c3f5232d70f3456565c5689b765d1001b046211
Willem Delventhal <will.delventhal@gmail.com>
2025-10-05 16:25:14 -0400

    Main menu switching functional

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject mainMenuObject;
+    [SerializeField] private GameObject howToPlayObject;
+    [SerializeField] private GameObject creditsObject;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -9,8 +14,34 @@ public class MainMenuManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void SwitchToMainMenu()
     {
-        
+        mainMenuObject.SetActive(true);
+        howToPlayObject.SetActive(false);
+        creditsObject.SetActive(false);
+    }
+
+    public void SwitchToHowToPlay()
+    {
+        mainMenuObject.SetActive(false);
+        howToPlayObject.SetActive(true);
+        creditsObject.SetActive(false);
+    }
+
+    public void SwitchToCedits()
+    {
+        mainMenuObject.SetActive(false);
+        howToPlayObject.SetActive(false);
+        creditsObject.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OpenDiscord()
+    {
+        Application.OpenURL("https://discord.gg/PQMKkBhMUv");
     }
 }
