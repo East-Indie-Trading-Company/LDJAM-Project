51228c777fb74f48f8453e070c5e1a6db3120acd
abcarso <100879194+ArwenSylveck@users.noreply.github.com>
2025-10-06 12:32:21 -0400

    Transition from main menu and stuff works now

---
+++ Assets/Scripts/MapLoadUI.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapLoadUI : MonoBehaviour
+{
+    
+    [SerializeField] private GameObject permaUICanvas;
+    void OnEnable()
+    {
+        Debug.Log("OnEnable called");
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    void OnDisable()
+    {
+        Debug.Log("OnDisable");
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Scene Loaded: " + scene.name);
+        if (scene.name == "Map")
+        {
+            permaUICanvas.SetActive(true);
+        }
+    }
+}
