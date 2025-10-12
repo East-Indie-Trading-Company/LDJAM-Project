a0cb4d819364d9afd77b183e327f9c8eb317c559
gamenuts86 <149576957+gamenuts86@users.noreply.github.com>
2025-10-12 05:54:28 +0100

    City Names

--- Assets/Scripts/UI/UIHoverScale.cs
+++ Assets/Scripts/UI/UIHoverScale.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -6,20 +7,38 @@ public class UIHoverScale : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     private Vector3 originalScale;
     private Vector3 hoverScale = new Vector3(.5f, .5f, .5f);
 
+    [Header("City Info")]
+    [SerializeField] private string cityName;                 // nome da cidade deste botão
+    [SerializeField] private TextMeshProUGUI cityNameText;    // referência ao texto do Canvas
+
+
     private void Awake()
     {
         originalScale = transform.localScale;
+
+        if (cityNameText != null)
+            cityNameText.gameObject.SetActive(false);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         transform.localScale = originalScale + hoverScale;
+
+        if (cityNameText != null)
+        {
+            cityNameText.text = cityName;
+            cityNameText.gameObject.SetActive(true);
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         transform.localScale = originalScale;
-    }
+
+        if (cityNameText != null)
+            cityNameText.gameObject.SetActive(false);
+    
+}
 
         public void OnPointerClick(PointerEventData eventData)
     {
